2b27f699f996b9b6f87a64d2ebaca3b74f810273
Arunpratap26 <arunpratap26@yahoo.com>
2023-01-15 00:53:12 +0530

    End of section 5

---
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            var walks = await walkRepository.GetAllAsync();
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walks);
+            return Ok(walksDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkAsync")]
+        public async Task<IActionResult> GetWalkAsync(Guid id)
+        {
+            var walk = await walkRepository.GetAsync(id);
+            var walksDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return Ok(walksDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkAsync([FromBody] AddWalkRequest addWalkRequest)
+        {
+            // Convert DTO to Domain Object
+            var walkDomain = new Models.Domains.Walk
+            {
+                Name = addWalkRequest.Name,
+                Length = addWalkRequest.Length,
+                RegionId = addWalkRequest.RegionId,
+                WalkDifficultyId = addWalkRequest.WalkDifficultyId
+            };
+            // Pass domain object to Repository to persist this
+            walkDomain = await walkRepository.AddAsync(walkDomain);
+
+            // Convert the Domain object back to DTO
+            var walkDTO = new Models.DTO.Walk
+            {
+                Id = walkDomain.Id,
+                Name = walkDomain.Name,
+                Length = walkDomain.Length,
+                RegionId = walkDomain.RegionId,
+                WalkDifficultyId = walkDomain.WalkDifficultyId
+            };
+
+            // Send DTO response back to Client
+            return CreatedAtAction(nameof(GetWalkAsync), new { id = walkDTO.Id }, walkDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkAsync([FromRoute] Guid id, [FromBody] Models.DTO.UpdateWalkRequest updateWalkRequest)
+        {
+            // Convert DTO to Domain object
+            var walkDomain = new Models.Domains.Walk
+            {
+                Length = updateWalkRequest.Length,
+                Name = updateWalkRequest.Name,
+                RegionId = updateWalkRequest.RegionId,
+                WalkDifficultyId = updateWalkRequest.WalkDifficultyId
+            };
+
+            // Pass details to Repository - Get Domain object in response (or null)
+            walkDomain = await walkRepository.UpdateAsync(id, walkDomain);
+
+            // Handle Null (not found)
+            if (walkDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Convert back Domain to DTO
+            var walkDTO = new Models.DTO.Walk
+            {
+                Id = walkDomain.Id,
+                Length = walkDomain.Length,
+                Name = walkDomain.Name,
+                RegionId = walkDomain.RegionId,
+                WalkDifficultyId = walkDomain.WalkDifficultyId
+            };
+
+            // Return Response
+            return Ok(walkDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkAsync(Guid id)
+        {
+            // call Repository to delete walk
+            var walkDomain = await walkRepository.DeleteAsync(id);
+
+            if (walkDomain == null)
+            {
+                return NotFound();
+            }
+
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walkDomain);
+
+            return Ok(walkDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Profiles/WalksProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class WalksProfile : Profile
+    {
+        public WalksProfile()
+        {
+            CreateMap<Models.Domains.Walk, Models.DTO.Walk>()
+                .ReverseMap();
+
+            CreateMap<Models.Domains.WalkDifficulty, Models.DTO.WalkDifficulty>()
+                .ReverseMap();
+        }
+    }
+}
